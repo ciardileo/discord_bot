68181ee6c1d523c98a89ca5e21e47068c35a1770
Léo Ciardi <leolopes.ciardi04@gmail.com>
2021-02-24 22:23:46 -0300

    requirements

---
+++ cogs/others.py
@@ -0,0 +1,21 @@
+# imports
+
+from discord.ext import commands
+import random
+
+
+class Others(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    # commands
+
+    # say hi
+    @commands.command(aliases=['eae', 'eaí', 'salve', 'opa', 'eai', 'olá', 'ola'])
+    async def oi(self, ctx):
+        greetings = ['Eaí', 'Eae', 'Oi', 'Olá', 'Salve', 'Opa,', 'Fala aí']
+        await ctx.send(f'{random.choice(greetings)} {ctx.author.mention} 👋🏻')
+
+
+def setup(client):
+    client.add_cog(Others(client))
