4dd02bda170dabbead033c92653efcb91e0bcc53
Léo Ciardi <leolopes.ciardi04@gmail.com>
2021-02-25 11:33:03 -0300

    adding cout command

--- cogs/math.py
+++ cogs/math.py
@@ -10,13 +10,30 @@ class Math(commands.Cog):
     # commands
 
     # take a "*" on parameters means that the next parameter has multiple arguments as one
-    # sum command
-    @commands.command()
-    async def soma(self, ctx, number1, number2):
-        try:
-            await ctx.send(f'{number1} + {number2} = {round(float(number1) + float(number2))}')
-        except:
-            await ctx.send('Você tem que colocar números!\nTenta de novo')
+    # count command
+
+    @commands.command(aliases=['count'])
+    async def conta(self, ctx, number1, operator, number2):
+        if operator == '+':
+            try:
+                await ctx.send(f'{number1} + {number2} = {round(float(number1) + float(number2))}')
+            except:
+                await ctx.send('Você tem que colocar números!\nTenta de novo')
+        if operator == '-':
+            try:
+                await ctx.send(f'{number1} - {number2} = {round(float(number1) - float(number2))}')
+            except:
+                await ctx.send('Você tem que colocar números!\nTenta de novo')
+        if operator == ':' or operator == '/':
+            try:
+                await ctx.send(f'{number1} : {number2} = {round(float(number1) / float(number2))}')
+            except:
+                await ctx.send('Você tem que colocar números!\nTenta de novo')
+        if operator == 'x' or operator == '.' or operator == '*':
+            try:
+                await ctx.send(f'{number1} x {number2} = {round(float(number1) * float(number2))}')
+            except:
+                await ctx.send('Você tem que colocar números!\nTenta de novo')
 
 
 def setup(client):
