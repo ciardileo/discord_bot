c4683ee5576e7691cf89f4bc85bbffa3433e03ee
Léo Ciardi <leolopes.ciardi04@gmail.com>
2021-09-14 08:40:55 -0300

    Starting the new development phase

--- bot.py
+++ bot.py
@@ -1,44 +1,31 @@
 # imports
-
 import os
 from itertools import cycle
 import discord
-# from discord import Embed
 from discord.ext import commands, tasks
 import databases as db
+from rich.console import Console
+from rich.progress import track
 
-# bot config
-# user discriminator = user code}
-
-# atributes
+# console
+console = Console()
 
 # bot
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
-client = commands.Bot(command_prefix='>', intents=intents)
+client = commands.Bot(command_prefix='c/', intents=intents)
 
 # status list
-status = cycle(['Hello World! Ainda estou em progresso, confira com o ADM as funções que eu já tenho.',
-                'Python. Sim essa é minha linguagem, ainda estou em progresso, continue testando para logo mais eu '
-                'ser o melhor bot de TODOS',
-                'Fica flinstons aí que eu to chegando', 'Eu não pareço um sarigue?',
-                'Não sou escravo de vocês, mas trabalho de grassa'])
-
+status = cycle(['Fica flinstons aí que eu to chegando', 'Eu não pareço um sarigue?', 'Dá um "c/help" pra ver o que o melhor bot do mundo tem a oferecer '])
 
 # events
 # def to be executed when the bot starts
-
 @client.event
 async def on_ready():
-	change_status.start()
-	# revive_chat.start()
+    change_status.start()
 
-	print('Online')
-	print(client.user.name)  # bot name
-	print(client.user.id)  # id bot
-
-	guild = client.get_guild(823546618347257877)
-	print()
-
+    console.log('[green]Bot Online[/]')
+    console.print(client.user.name)  # bot name
+    console.print(f'ID: {client.user.id}')  # id bot
 
 # embed = Embed(title='O Pai Ta On Família!', description='Super Bot Opressor está online.', colour=0xFF0000)
 # fields = [('Meus comandos', 'Mano, dá um `>help` pra saber mais', True),
@@ -51,33 +38,29 @@ async def on_ready():
 #                  icon_url=guild.icon_url)
 # await channel.send(embed=embed)
 
-
 # commands
-
 # load a cog
 @client.command()
 async def load(ctx, extension):
-	client.load_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
 
 
 # unload a cog
 @client.command()
 async def unload(ctx, extension):
-	client.unload_extension(f'cogs.{extension}')
+    client.unload_extension(f'cogs.{extension}')
 
 
 # load all cogs when the bot starts
 for filename in os.listdir('./cogs'):
-	if filename.endswith('.py'):
-		client.load_extension(f'cogs.{filename[:-3]}')
-
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
 # background task that will be executed every 10 seconds
-
 # change status
 @tasks.loop(seconds=10)
 async def change_status():
-	await client.change_presence(activity=discord.Game(next(status)))
+    await client.change_presence(activity=discord.Game(next(status)))
 
 
 # run the bot
--- cogs/log.py
+++ cogs/log.py
@@ -11,110 +11,110 @@ class Logs(commands.Cog):
         self.client = client
 
     # on member update
-    @commands.Cog.listener()
-    async def on_user_update(self, before, after):
-        channel = self.client.get_channel(823881567893585960)
-        if before.avatar_url != after.avatar_url:
-            embed = Embed(title='Membro mudou perfil', description='Mudou foto de perfil (a de baixo é a nova)',
-                          colour=after.colour, timestamp=datetime.utcnow())
-            embed.set_image(url=after.avatar_url)
-            embed.set_thumbnail(url=before.avatar_url)
-            embed.set_author(name=after.name, icon_url=after.avatar_url)
-            await channel.send(embed=embed)
-        if before.name != after.name:
-            embed = Embed(title='Membro mudou o perfil', description='Mudou o nome', colour=after.colour,
-                          timestamp=datetime.utcnow())
-            embed.add_field(name='Antes', value=f'```{before.name}```', inline=False)
-            embed.add_field(name='Depois', value=f'```{after.name}```', inline=False)
-            await channel.send(embed=embed)
-
-    # on member update
-    @commands.Cog.listener()
-    async def on_member_update(self, before, after):
-        channel = self.client.get_channel(823881567893585960)
-        if before.display_name != after.display_name:
-
-            embed = Embed(title=f'Membro mudou o perfil', description='Mudou apelido', colour=after.colour,
-                          timestamp=datetime.utcnow())
-
-            embed.set_author(name=f'{before.name}#{before.discriminator}', icon_url=before.avatar_url)
-            embed.set_footer(text=f'ID do usuário: {before.id}')
-
-            fields = [('Antes', f'```{before.display_name}```', False),
-                      ('Depois', f'```{after.display_name}```', False)]
-
-            for name, value, inline in fields:
-                embed.add_field(name=name, value=value, inline=inline)
-
-            print(channel)
-
-            await channel.send(embed=embed)
-
-        elif before.roles != after.roles:
-            channel = self.client.get_channel(823881567893585960)
-            embed = Embed(title=f'Membro mudou o perfil', description='Mudou de cargo', colour=after.colour,
-                          timestamp=datetime.utcnow())
-
-            embed.set_author(name=f'{before.name}#{before.discriminator}', icon_url=before.avatar_url)
-            embed.set_footer(text=f'ID do usuário: {before.id}')
-
-            fields = [('Antes', '\n'.join([role.mention for role in before.roles]), False),
-                      ('Depois', '\n'.join([role.mention for role in after.roles]), False)]
-
-            # list comprehension
-
-            for name, value, inline in fields:
-                embed.add_field(name=name, value=value, inline=inline)
-
-            print(channel)
-
-            await channel.send(embed=embed)
-
-    # on message edit
-    @commands.Cog.listener()
-    async def on_message_edit(self, before, after):
-        if not after.author.bot:
-            channel = self.client.get_channel(823881567893585960)
-            if before.content != after.content:
-
-                embed = Embed(title=f'{after.author.name} editou a mensagem',
-                              description=f'Mensagem editada no canal: {after.channel.mention}',
-                              colour=after.author.colour,
-                              timestamp=datetime.utcnow())
-
-                embed.set_author(name=f'{before.author.name}#{before.author.discriminator}',
-                                 icon_url=before.author.avatar_url)
-                embed.set_footer(text=f'ID do usuário: {before.author.id}')
-
-                fields = [('Antes', f'```{before.content}```', False),
-                          ('Depois', f'```{after.content}```', False)]
-
-                for name, value, inline in fields:
-                    embed.add_field(name=name, value=value, inline=inline)
-
-                print(channel)
-
-                await channel.send(embed=embed)
-
-    # on message delete
-    @commands.Cog.listener()
-    async def on_message_delete(self, message):
-        if not message.author.bot:
-            channel = self.client.get_channel(823881567893585960)
-            embed = Embed(title=f'{message.author.name} deletou uma mensagem',
-                          description=f'Mensagem deletada no canal: {message.channel.mention}',
-                          colour=message.author.colour,
-                          timestamp=datetime.utcnow())
-
-            embed.set_author(name=f'{message.author.name}#{message.author.discriminator}',
-                             icon_url=message.author.avatar_url)
-            embed.set_footer(text=f'ID do usuário: {message.author.id}')
-
-            embed.add_field(name='Mensagem', value=f'```{message.content}```', inline=False)
-
-            print(channel)
-
-            await channel.send(embed=embed)
+    # @commands.Cog.listener()
+    # async def on_user_update(self, before, after):
+    #     channel = self.client.get_channel(823881567893585960)
+    #     if before.avatar_url != after.avatar_url:
+    #         embed = Embed(title='Membro mudou perfil', description='Mudou foto de perfil (a de baixo é a nova)',
+    #                       colour=after.colour, timestamp=datetime.utcnow())
+    #         embed.set_image(url=after.avatar_url)
+    #         embed.set_thumbnail(url=before.avatar_url)
+    #         embed.set_author(name=after.name, icon_url=after.avatar_url)
+    #         await channel.send(embed=embed)
+    #     if before.name != after.name:
+    #         embed = Embed(title='Membro mudou o perfil', description='Mudou o nome', colour=after.colour,
+    #                       timestamp=datetime.utcnow())
+    #         embed.add_field(name='Antes', value=f'```{before.name}```', inline=False)
+    #         embed.add_field(name='Depois', value=f'```{after.name}```', inline=False)
+    #         await channel.send(embed=embed)
+    #
+    # # on member update
+    # @commands.Cog.listener()
+    # async def on_member_update(self, before, after):
+    #     channel = self.client.get_channel(823881567893585960)
+    #     if before.display_name != after.display_name:
+    #
+    #         embed = Embed(title=f'Membro mudou o perfil', description='Mudou apelido', colour=after.colour,
+    #                       timestamp=datetime.utcnow())
+    #
+    #         embed.set_author(name=f'{before.name}#{before.discriminator}', icon_url=before.avatar_url)
+    #         embed.set_footer(text=f'ID do usuário: {before.id}')
+    #
+    #         fields = [('Antes', f'```{before.display_name}```', False),
+    #                   ('Depois', f'```{after.display_name}```', False)]
+    #
+    #         for name, value, inline in fields:
+    #             embed.add_field(name=name, value=value, inline=inline)
+    #
+    #         print(channel)
+    #
+    #         await channel.send(embed=embed)
+    #
+    #     elif before.roles != after.roles:
+    #         channel = self.client.get_channel(823881567893585960)
+    #         embed = Embed(title=f'Membro mudou o perfil', description='Mudou de cargo', colour=after.colour,
+    #                       timestamp=datetime.utcnow())
+    #
+    #         embed.set_author(name=f'{before.name}#{before.discriminator}', icon_url=before.avatar_url)
+    #         embed.set_footer(text=f'ID do usuário: {before.id}')
+    #
+    #         fields = [('Antes', '\n'.join([role.mention for role in before.roles]), False),
+    #                   ('Depois', '\n'.join([role.mention for role in after.roles]), False)]
+    #
+    #         # list comprehension
+    #
+    #         for name, value, inline in fields:
+    #             embed.add_field(name=name, value=value, inline=inline)
+    #
+    #         print(channel)
+    #
+    #         await channel.send(embed=embed)
+    #
+    # # on message edit
+    # @commands.Cog.listener()
+    # async def on_message_edit(self, before, after):
+    #     if not after.author.bot:
+    #         channel = self.client.get_channel(823881567893585960)
+    #         if before.content != after.content:
+    #
+    #             embed = Embed(title=f'{after.author.name} editou a mensagem',
+    #                           description=f'Mensagem editada no canal: {after.channel.mention}',
+    #                           colour=after.author.colour,
+    #                           timestamp=datetime.utcnow())
+    #
+    #             embed.set_author(name=f'{before.author.name}#{before.author.discriminator}',
+    #                              icon_url=before.author.avatar_url)
+    #             embed.set_footer(text=f'ID do usuário: {before.author.id}')
+    #
+    #             fields = [('Antes', f'```{before.content}```', False),
+    #                       ('Depois', f'```{after.content}```', False)]
+    #
+    #             for name, value, inline in fields:
+    #                 embed.add_field(name=name, value=value, inline=inline)
+    #
+    #             print(channel)
+    #
+    #             await channel.send(embed=embed)
+    #
+    # # on message delete
+    # @commands.Cog.listener()
+    # async def on_message_delete(self, message):
+    #     if not message.author.bot:
+    #         channel = self.client.get_channel(823881567893585960)
+    #         embed = Embed(title=f'{message.author.name} deletou uma mensagem',
+    #                       description=f'Mensagem deletada no canal: {message.channel.mention}',
+    #                       colour=message.author.colour,
+    #                       timestamp=datetime.utcnow())
+    #
+    #         embed.set_author(name=f'{message.author.name}#{message.author.discriminator}',
+    #                          icon_url=message.author.avatar_url)
+    #         embed.set_footer(text=f'ID do usuário: {message.author.id}')
+    #
+    #         embed.add_field(name='Mensagem', value=f'```{message.content}```', inline=False)
+    #
+    #         print(channel)
+    #
+    #         await channel.send(embed=embed)
 
 
 # load the cog
